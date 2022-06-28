d1cd8a461f302f7c9efdbb88f1a695d0055342f6
Nickolay Stepanov <niskoflywork@gmail.com>
2022-06-28 18:39:29 +0300

    [add] useSTATE

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,28 @@
-
+import React from "react";
 function Categories(){
+
+    const [activeIndex, setActiveIndex] = React.useState(0)
+
+    const onClickCategory = (index) =>{
+        setActiveIndex(index)
+    }
+
+    const categories = [
+        'Домики',
+        'Трубы',
+        'Страховочные',
+        'Двух-этажные',
+        'Гамаки-норки',
+        'Трех-этажные',
+    ]
+
     return(
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {
+                    categories.map((value, index) =>
+                        <li onClick={() => onClickCategory(index)} className={activeIndex === index ? 'active' : ''}>{value}</li>)
+                }
             </ul>
         </div>
     )
